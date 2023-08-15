3ce5e33c4c453b9fce31204c12c99e007ca932f5
WeiCheng <drchenweicheng@gmail.com>
2023-08-14 20:29:14 -0700

    problems 09-10

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -30,12 +30,15 @@ console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
 function chainMap(val, ...callbacks) {
-  // Your code here
+  for (let i = 0; i < callbacks.length; i++) {
+    value = callbacks[i](value)
+  }
+  return value
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = chainMap;
-} catch(e) {
+} catch (e) {
   return null;
 }
